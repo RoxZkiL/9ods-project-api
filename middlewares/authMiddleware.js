a03dc1156e443555d959ef902a088fbf3577d5e8
RoxZkiL <mttrox@gmail.com>
2024-08-13 10:27:00 -0400

    middleware authentication

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const authenticate = (req, res, next) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Authorization denied, empty token" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Token is not valid", error });
+  }
+};
+
+module.exports = authenticate;
