6b14027a5fe56e3f5d26648260c777aa20f0a743
RoxZkiL <mttrox@gmail.com>
2024-08-13 10:17:43 -0400

    login controller

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,3 +1,31 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findByEmail(email);
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: " Invalid email or password" });
+    }
+
+    const token = jew.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
+
+module.exports = login;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+require("dotenv").config();
 const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
-const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
